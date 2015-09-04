0792d83d379a023a516acbf8059f156f65dcd9b8
Nick Veys <nick@codelever.com>
2015-09-04 10:38:59 -0500

    Updating Rakefile, rspec files

--- Rakefile
+++ Rakefile
@@ -1,1 +1,11 @@
-require "bundler/gem_tasks"
+#!/usr/bin/env rake
+require 'bundler/gem_tasks'
+require 'rspec/core/rake_task'
+
+RSpec::Core::RakeTask.new('spec')
+task default: :spec
+
+if ENV['GENERATE_REPORTS'] == 'true'
+  require 'ci/reporter/rake/rspec'
+  task spec: 'ci:setup:rspec'
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,22 @@
-$LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
+require 'simplecov'
+require 'simplecov-gem-adapter'
+require 'simplecov-rcov'
+SimpleCov.formatter = SimpleCov::Formatter::RcovFormatter
+SimpleCov.start 'gem' if ENV['COVERAGE']
+
+require 'rspec/collection_matchers'
+require 'rspec/its'
+
+require 'awesome_print'
 require 'ipify'
+
+RSpec.configure do |config|
+  config.run_all_when_everything_filtered = true
+  config.filter_run :focus
+
+  # Run specs in random order to surface order dependencies. If you find an
+  # order dependency and want to debug it, you can fix the order by providing
+  # the seed, which is printed after each run.
+  #     --seed 1234
+  config.order = 'random'
+end
