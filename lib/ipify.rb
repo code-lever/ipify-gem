3b90c5c381aece3c85e2c9cea5c87e9fbd76b8de
Nick Veys <nick@codelever.com>
2015-09-04 14:57:15 -0500

    Adding Ipify.ip! to get exceptions back

--- lib/ipify.rb
+++ lib/ipify.rb
@@ -4,7 +4,19 @@ require 'retriable'
 
 module Ipify
 
+  # Requests this machine's IP address from the ipify.org service.  Will retry up to 3 times
+  # until ultimately returning +nil+.
+  #
+  # @return [String] external ip address, or +nil+ if unsuccessful
   def self.ip
+    ip! rescue nil
+  end
+
+  # Requests this machine's IP address from the ipify.org service.  Will retry up to 3 times
+  # until ultimately raising the last exception encountered.
+  #
+  # @return [String] external ip address
+  def self.ip!
     http = build_http
     Retriable.retriable tries: 3 do
       http.get('/').body
@@ -13,6 +25,7 @@ module Ipify
 
   private
 
+  # @return [Net::HTTP] http client
   def self.build_http
     uri = URI('https://api.ipify.org')
     http = Net::HTTP.new(uri.host, uri.port)
