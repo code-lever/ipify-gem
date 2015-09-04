eb2010a96d4fe97c4ffae31d145727a752acc142
Nick Veys <nick@codelever.com>
2015-09-04 15:17:36 -0500

    Adding exceptions that follow other library conventions

--- lib/ipify.rb
+++ lib/ipify.rb
@@ -4,6 +4,9 @@ require 'retriable'
 
 module Ipify
 
+  ConnectionError = Class.new(StandardError)
+  ServiceError = Class.new(StandardError)
+
   # Requests this machine's IP address from the ipify.org service.  Will retry up to 3 times
   # until ultimately returning +nil+.
   #
@@ -16,11 +19,17 @@ module Ipify
   # until ultimately raising the last exception encountered.
   #
   # @return [String] external ip address
+  # @raise [Ipify::ConnectionError] if unable to reach the ipify service
+  # @raise [Ipify::ServiceError] if the ipify service returns status other than 200(OK)
   def self.ip!
     http = build_http
-    Retriable.retriable tries: 3 do
-      http.get('/').body
+    response = Retriable.retriable tries: 3 do
+      http.get('/')
     end
+    raise ServiceError, "ipify.org returned status #{response.code}" if '200' != response.code
+    response.body
+  rescue Net::OpenTimeout
+    raise ConnectionError, "ipify.org timed out"
   end
 
   private
@@ -30,8 +39,8 @@ module Ipify
     uri = URI('https://api.ipify.org')
     http = Net::HTTP.new(uri.host, uri.port)
     http.use_ssl = true
-    http.open_timeout = 10
-    http.read_timeout = 5
+    http.open_timeout = 1
+    http.read_timeout = 1
     http
   end
 
